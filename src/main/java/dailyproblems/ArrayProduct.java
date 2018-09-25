01bb43958bcbf767ffb7e84408cb8d87cb02b1ab
Alberto <alberto@aramirez.es>
2018-09-25 20:33:16 +0200

    Using streams instead of arrays

--- src/main/java/dailyproblems/ArrayProduct.java
+++ src/main/java/dailyproblems/ArrayProduct.java
@@ -1,17 +1,11 @@
 package dailyproblems;
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 class ArrayProduct {
   int[] execute(int[] numbers) {
-    int [] output = new int[numbers.length];
-
-    int sum = Arrays.stream(numbers).reduce(1, (left, right) -> left * right);
-
-    for (int i = 0; i < numbers.length; i++) {
-      output[i] = sum / numbers[i];
-    }
-
-    return output;
+    int product = Arrays.stream(numbers).reduce(1, (left, right) -> left * right);
+    return IntStream.range(0, numbers.length).map(i -> product / numbers[i]).toArray();
   }
 }
